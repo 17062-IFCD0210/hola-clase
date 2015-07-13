95c5b9d79223ccfe332da55e2c5d65d34eac960b
javi70 <javi70@gmail.com>
2015-07-13 15:55:25 +0200

    casa dia 13

--- src/java/com/ipartek/formacion/holaclase/poo/EjercicioVueltas.java
+++ src/java/com/ipartek/formacion/holaclase/poo/EjercicioVueltas.java
@@ -9,13 +9,30 @@ public class EjercicioVueltas {
 	
 	public static void main(String[] args) {
 		
-		float cambio = valorCompra - dineroPagado;
+		float cambio = dineroPagado - valorCompra;
 		int indice = 0;
+		float parteEntera=0f;
+		float resto=0f;
 		
-		while(){
+		System.out.println("------------- RESULTADO ---------------------");
+		System.out.println("Vueltas de " + cambio + " Euros:");
+		
+		
+		do{
+			parteEntera=(int) (cambio/BILLETES_MONEDAS[indice]);
+			resto=cambio%BILLETES_MONEDAS[indice];
+			if(indice<=3){
+				System.out.println("En " + (int)parteEntera + " Billetes de " + (int)BILLETES_MONEDAS[indice] + "   resto:" + resto);
+			}
+			else{
+				System.out.println("En " + (int)parteEntera + " Monedas de " + (int)BILLETES_MONEDAS[indice] + "   resto:" + resto);
+			}
+			cambio=resto;
 			indice++;
 		}
-		
+		while(cambio>0 && indice<11);
+		System.out.println("---------------------------------------------");
+
 	}
 	
 }
