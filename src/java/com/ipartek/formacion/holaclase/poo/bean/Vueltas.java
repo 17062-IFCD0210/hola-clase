99baffcebae5cc83c5741a1b026700ea247444e0
crismartin666 <crismartin666@hotmail.es>
2015-07-13 13:35:25 +0200

    ejercicio vueltas del campus

---
+++ src/java/com/ipartek/formacion/holaclase/poo/bean/Vueltas.java
@@ -0,0 +1,16 @@
+package com.ipartek.formacion.holaclase.poo.bean;
+
+public class Vueltas {
+	
+	public final float [] billetes_monedas = new float[100];
+	
+	//Billetes de 50
+	billetes_monedas[0] = 
+	
+	//
+	
+	
+	
+	
+
+}
