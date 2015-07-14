27b60171f5ed950d99d7f6210c58ac8f695a40ae
rgarciaf92 <raulgf1992@gmail.com>
2015-07-14 11:02:47 +0200

    Ejercicio Vueltas completo(Calculadora)

---
+++ src/java/com/ipartek/formacion/holaclase/poo/Calculadora.java
@@ -0,0 +1,105 @@
+package com.ipartek.formacion.holaclase.poo;
+
+import java.math.BigDecimal;
+
+/**
+ * Objeto para calcular las vueltas de un cobro de forma mas optima posible
+ * 
+ * @author Raul Garcia
+ *
+ */
+public class Calculadora {
+
+	/**
+	 * precio de la compra
+	 */
+	private float precio;
+
+	/**
+	 * pago realizado o dinero entregado
+	 */
+	private float pago;
+
+	/**
+	 * Array con las vueltas
+	 */
+	private int[] aVueltas = new int[BILLETES_MONEDAS.length];
+
+	/**
+	 * Array con tipos de Monedas y Billetes
+	 * <ul>
+	 * <li>billete 50€</li>
+	 * <li>billete 20€</li>
+	 * <li>billete 10€</li>
+	 * <li>billete 5€</li>
+	 * <li>moneda 50€</li>
+	 * <li>etc...</li>
+	 * </ul>
+	 */
+	public static final float[] BILLETES_MONEDAS = { 50f, 20f, 10f, 5f, 2f, 1f,
+			0.50f, 0.20f, 0.10f, 0.05f, 0.02f, 0.01f };
+
+	/**
+	 * valor en euros del billete de menor tamaño
+	 */
+	static final int BILLETE_MINIMO = 5;
+
+	public Calculadora() {
+	}
+
+	/**
+	 * Obtener las vueltas calculadas, para saber el valor de cada posicion del
+	 * array podemos usar la variable: public static final int[]
+	 * BILLETES_MONEDAS
+	 * 
+	 * @return {@code array} de {@code int} con las vueltas
+	 */
+	public int[] getVueltas() {
+		return aVueltas;
+	}
+
+	/**
+	 * Calcula las vueltas de forma optima para retornar los minimos billetes y
+	 * monedas posibles
+	 * 
+	 * @param pago
+	 *            {@code float} dinero entregado o pagado
+	 * @param precio
+	 *            {@code float} precio a cobrar
+	 */
+	public void calcular(float pago, float precio) {
+		this.pago = pago;
+		this.precio = precio;
+		float resto = this.pago - this.precio;	
+		for(int i=0; i<BILLETES_MONEDAS.length; i++) {
+			aVueltas[i] = (int) (resto/BILLETES_MONEDAS[i]);
+			resto %= BILLETES_MONEDAS[i];
+			resto = round(resto,2);
+		}
+	}
+
+	/**
+	 * Imprimir por pantalla las vueltas del cobro
+	 */
+	public void imprimirVueltas() {
+		System.out.println("-----RESULTADO-------");
+		for(int i=0; i<BILLETES_MONEDAS.length; i++) {
+			if(i<=3) {
+				System.out.println(aVueltas[i] + " billetes de " + BILLETES_MONEDAS[i] );
+			} else {
+				System.out.println(aVueltas[i] + " monedas de " + BILLETES_MONEDAS[i] );
+			}
+		}
+	}
+	/**
+	 * Funcion que redondea los decimales largos
+	 * @param d numero decimal a redondear
+	 * @param decimalPlace numero de decimales a redondear
+	 * @return el valor decimal con los decimales deseados
+	 */
+	public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+}
--- src/java/com/ipartek/formacion/holaclase/poo/bean/Vueltas.java
+++ src/java/com/ipartek/formacion/holaclase/poo/bean/Vueltas.java
@@ -1,26 +1,12 @@
 package com.ipartek.formacion.holaclase.poo.bean;
 
+import com.ipartek.formacion.holaclase.poo.Calculadora;
+
 public class Vueltas {
-	public static float[] BILLETES_MONEDAS = {50f,20f,10f,5f,2f,1f,0.5f,0.2f,0.1f, 0.05f, 0.02f, 0.01f};
-	public static float PAGO = 2000f;
-	public static float PRECIO = 1326.24f;
-	//Atributos
-	private int billetes50 = 0;
-	private int billetes20 = 0;
-	private int billetes10 = 0;
-	private int billetes5 = 0;
-	private int monedas2 = 0;
-	private int monedas1 = 0;
-	private int monedas05 = 0;
-	private int monedas02 = 0;
-	private int monedas01 = 0;
-	private int monedas005 = 0;
-	private int monedas002 = 0;
-	private int monedas001 = 0;
 	
-	//Constructores
-	
-	//Getters y Setters
-	
-	//Metodos
+	public static void main(String[] args){
+		Calculadora calc = new Calculadora();
+		calc.calcular(2000f, 1326.24f);
+		calc.imprimirVueltas();
+	}
 }
