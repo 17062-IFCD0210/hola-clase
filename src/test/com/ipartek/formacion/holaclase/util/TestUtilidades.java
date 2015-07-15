b0d37a1086d5544274edc2d1aa65b8ec72e6cd81
ieltxuorue <ieltxuorue@hotmail.com>
2015-07-15 08:51:48 +0200

    Añadido Test para clase Utilidades

--- src/test/com/ipartek/formacion/holaclase/util/TestUtilidades.java
+++ src/test/com/ipartek/formacion/holaclase/util/TestUtilidades.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.holaclase.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -16,7 +17,23 @@ public class TestUtilidades {
 
 	@Test
 	public void testOrdenar() {
-		
+		int[] n = new int[]{3, 55, 0, 9, -2, 14};
+		int[] nAscendente = Utilidades.ordenar(n, 0);
+		int[] nDescendente = Utilidades.ordenar(n, 1);
+
+		assertEquals(-2, nAscendente[0]);
+		assertEquals( 0, nAscendente[1]);
+		assertEquals( 3, nAscendente[2]);
+		assertEquals( 9, nAscendente[3]);
+		assertEquals(14, nAscendente[4]);
+		assertEquals(55, nAscendente[5]);
+			
+		assertEquals(55, nDescendente[0]);
+		assertEquals(14, nDescendente[1]);
+		assertEquals( 9, nDescendente[2]);
+		assertEquals( 3, nDescendente[3]);
+		assertEquals( 0, nDescendente[4]);
+		assertEquals(-2, nDescendente[5]);
 	}
 	
 }
