62f1d0f52247109f33b6081e93e506fe7d38f146
Ander Ipartek <auraga@ipartek.com>
2015-07-20 10:20:49 +0200

    Ejemplo herencia con vehiculos

---
+++ src/java/com/ipartek/formacion/holaclase/poo/bean/VehiculoElectrico.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.holaclase.poo.bean;
+
+public class VehiculoElectrico extends Vehiculo {
+
+	/* Tiene todos los atributos del Padre mas los usyos propios */	
+	/* Los del padre no hace falta definirlos de nuevo, se heredan!!! */
+	private int capacidadBaterias;
+	
+	
+	/* Hemos usado el polimorfismo para que al construir un 
+	 * VehiculoElectrico la potencia minima sea 100 en vez de 0 */
+	public VehiculoElectrico() {
+		super();
+		this.setPotencia(100); 
+		this.capacidadBaterias = 300;
+	}
+
+
+	public int getCapacidadBaterias() {
+		return capacidadBaterias;
+	}
+
+
+	public void setCapacidadBaterias(int capacidadBaterias) {
+		this.capacidadBaterias = capacidadBaterias;
+	}
+
+
+	@Override
+	public String arrancar() {		
+		return "Check bateria - " + super.arrancar();
+	}
+
+	public boolean checkBateria(){
+		return true;
+	}; 
+		
+	
+	
+}
