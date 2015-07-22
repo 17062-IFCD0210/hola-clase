3e6b2b4e3b3281f0a237fd5f13476cbe6a7bee70
Ander Ipartek <auraga@ipartek.com>
2015-07-22 12:01:00 +0200

    escribir fichero

--- src/test/com/ipartek/formacion/holaclase/poo/TestFicheros.java
+++ src/test/com/ipartek/formacion/holaclase/poo/TestFicheros.java
@@ -2,6 +2,7 @@ package com.ipartek.formacion.holaclase.poo;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -19,6 +20,11 @@ public class TestFicheros {
 	static final String PATH_FILES = "files/";
 	static final String PATH_FICHERO1 = PATH_FILES + "fichero1.txt";
 	static final String PATH_FICHERO2 = PATH_FILES + "fichero2.txt";
+	static final String PATH_FICHERO_GORDO = PATH_FILES + "fichero_gordo.txt";
+	
+	//1º parrafo de LoremIpsum
+	static final String PARRAFO = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod ligula in massa semper vestibulum. Aliquam sed ex risus. Quisque lorem quam, fringilla id neque vitae, facilisis tempor augue. Quisque ullamcorper bibendum erat, ac rutrum tortor posuere vitae. Quisque convallis pretium lacinia. Morbi porttitor leo vitae suscipit luctus. Aliquam sed feugiat dui, at dictum ex. Maecenas ac posuere urna. Mauris at aliquam velit. Curabitur dapibus fermentum lorem. Morbi molestie urna nec quam porta, ut maximus neque tincidunt. Maecenas dictum a justo nec tincidunt. Aliquam condimentum, tellus ac mollis hendrerit, dui orci lacinia nisl, tempor aliquet lorem nibh in nunc. Mauris nec leo mauris. Ut dictum ipsum eros, ut commodo ante ornare eu.";
+	
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -48,6 +54,28 @@ public class TestFicheros {
 		}
 		
 	}
+	
+	@Test
+	public void testLeerPrimerChar() {
+		try {
+			FileReader inputStream = new FileReader(PATH_FICHERO1);
+			
+			//leemos primer caracter
+			int c =	inputStream.read();
+			assertEquals('L', (char)c );		
+			
+			//saltamos 6 caracteres y volvemos a leer			
+			inputStream.skip(6);
+			c = inputStream.read();
+			assertEquals('p', (char)c );		
+				
+			
+		} catch (Exception e) {
+			fail("Excepcion leyendo el primer caracter" );
+			e.printStackTrace();
+		}
+	}
+	
 	
 	@Test
 	public void testCopiar() {
@@ -85,5 +113,49 @@ public class TestFicheros {
 		}
 		
 	}
+	
+	
+	
+	@Test(timeout=1000*4)
+	public void testCrearFichero() {
+				
+		FileWriter outputStream = null;
+		BufferedWriter bfw = null;
+		
+		try {
+			
+			outputStream = new FileWriter(PATH_FICHERO_GORDO);
+			bfw = new BufferedWriter( outputStream );
+			
+			for ( int i=0; i < 1000000; i++){
+				bfw.write( PARRAFO );
+			}	
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			fail("No se ha podido copiar el fichero " + PATH_FICHERO1 + " al fichero " + PATH_FICHERO2 );
+			
+		} catch (IOException e) {			
+			e.printStackTrace();
+			fail("No se ha podido escribir en el fichero " + PATH_FICHERO2 );
+		}finally{
+			
+			try{
+				
+				if ( bfw != null ){bfw.close();}
+				if ( outputStream != null ){outputStream.close();}
+				
+				
+			}catch(Exception e){
+				fail("Cerrando los Streams");
+			}	
+			
+		}
+		
+	}
+	
+	
 
 }
