d9f58bf7115c14ab7a258d29de57b0c03fb7217d
rgarciaf92 <raulgf1992@gmail.com>
2015-07-22 09:11:27 +0200

    Crear Fichero para parsear HTML

--- src/java/com/ipartek/formacion/holaclase/parse/JsoupParse.java
+++ src/java/com/ipartek/formacion/holaclase/parse/JsoupParse.java
@@ -1,74 +1,69 @@
 package com.ipartek.formacion.holaclase.parse;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class JsoupParse {
 
-	static final String URL = "http://zonasdeescalada.com/zonas?title=&field_comunidad_ciudad_tid=163&field_escuela_mejor_epoca_tid=All&field_escuela_calidad_zona_tid_1=All&field_escuela_roca_tid=All&field_tipos_de_escalada_tid=All";
-	static final String PATH = "c:/prueba.txt";
+	static final String URL = "http://zonasdeescalada.com/zonas?title=&field_comunidad_ciudad_tid=211&field_escuela_mejor_epoca_tid=All&field_escuela_calidad_zona_tid_1=All&field_escuela_roca_tid=All&field_tipos_de_escalada_tid=All";
+	static final String FILE_FOLDER = "files/"; // carpeta a guardar html
+	// descargados
+	static final String FILE_EXTENSION = ".html";
 
 	public static void main(String[] args) {
 
 		System.out.println("Comenzamos parseo: " + URL);
 
 		Document doc;
-		FileWriter fichero = null;
-		PrintWriter pw = null;
+		String titlePagina = "";
+		// ArrsayList<String> enlaces = new ArrayAllocationExpression()
+
 		try {
 			doc = Jsoup.connect(URL).get();
 			System.out.println("Conectado");
 
-			fichero = new FileWriter("c:/prueba.txt");
-			new PrintWriter(fichero);
+			// obtener el codigo fuente
+			// System.out.println( doc.html() );
 
-			pw.println(doc.html());
+			titlePagina = doc.title();
+			System.out.println("Title= " + titlePagina);
 
+			final File f = new File(FILE_FOLDER + titlePagina + FILE_EXTENSION);
+			FileUtils.writeStringToFile(f, doc.outerHtml(), "UTF-8");
+
+			Elements enlaces = doc.getElementsByTag("a");
+			System.out.println("Enlaces encontrados= " + enlaces.size());
+			// System.out.println(enlaces);
+			Element enlace = null;
+			String urlSalto = null;
+
+			for (int i = 0; i < enlaces.size(); i++) {
+				enlace = enlaces.get(i);
+				urlSalto = enlace.attr("href");
+				// System.out.println(urlSalto);
+				if (urlSalto != null && !"".equals(urlSalto)) {
+
+					doc = Jsoup.connect(urlSalto).get();
+					titlePagina = doc.title();
+					System.out.println("    Title= " + titlePagina);
+
+				} else {
+					System.out.println("   *No se ha encontrado la url");
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				// Nuevamente aprovechamos el finally para
-				// asegurarnos que se cierra el fichero.
-				if (null != fichero)
-					fichero.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
 		}
 
-		// titlePagina = doc.title();
-		// System.out.println("Title = " + titlePagina);
-		//
-		// Elements enlaces = doc.getElementsByTag("a");
-		// System.out.println("Enlaces encontrados: " + enlaces.size());
-		//
-		// // System.out.println(enlaces);
-		// Element enlace = null;
-		// String urlSalto = null;
-		// for (int i = 0; i < enlaces.size(); i++) {
-		// enlace = enlaces.get(i);
-		// urlSalto = enlace.attr("href");
-		// System.out.println(urlSalto);
-		// // if (urlSalto != null && !("".equals(urlSalto))) {
-		// // doc = Jsoup.connect(urlSalto).get();
-		// //
-		// // titlePagina = doc.title();
-		// // System.out.println("	Title = " + titlePagina);
-		// // } else {
-		// // System.out.println("No se ha encontrado la URL");
-		// // }
-		// }
-		//
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
 		System.out.println("Terminamos parseo");
 
 	}
